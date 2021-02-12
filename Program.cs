4ed6afce606446cf876c8f00efe07b9547de8003
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-12 09:57:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
+namespace SizeControl
+{
+    class Program
+    {
+        static void Main()
+        {
+            Size size = new Size();
+            size.Height = 600;
+            size.Width = 600;
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://testing.todorvachev.com/");
+            driver.Manage().Window.Size = size;
+        }
+    }
+}
